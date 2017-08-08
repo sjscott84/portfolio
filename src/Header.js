19ef415c099943fd68e953d41547a24a2eefda18
Sarah Scott <s.j.scott84@gmail.com>
2017-08-08 14:29:09 +1200

    Update home page to show projects

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,6 @@ import { Link } from 'react-router-dom';
 import './App.css';
 
 class Header extends Component {
-  displayProject(){
-    if(location.hash !== "#/projects"){
-      return <p className="header-text"><Link to="/projects">Projects</Link></p>
-    }else{
-      return <p className="header-text"><Link to="/">Home</Link></p>
-    }
-  };
   displayAbout(){
     if(location.hash !== "#/about"){
       return <p className="header-text"><Link to="/about">About Me</Link></p>
@@ -28,7 +21,6 @@ class Header extends Component {
     return (
       <div className="header">
         <div className="headerLinks">
-          {this.displayProject()}
           {this.displayAbout()}
           {this.displayContact()}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import { Link } from 'react-router-dom';
+import Projects from './Projects';
 import './App.css';
 
 class Home extends Component {
@@ -12,9 +12,9 @@ class Home extends Component {
           <img src={require("./images/me.jpg")} alt="Sarah" className="photo" />
           <h1 className="intro-header">Hello</h1>
           <p className="intro">
-            I'm Sarah and I am a self taught developer who loves exploring and experimenting with code.  Why don't you take a look at what I have been up to?
+            I'm Sarah and I am a self taught developer who loves exploring and experimenting with code and this is what I have been up to.
           </p>
-          <p className="project-link"><Link to="/projects">Projects</Link></p>
+          <Projects />
         </div>
       </div>
     );
